cf89b938388576741d62eae7e5984ee7c9652ca2
Salil V Nair <salilvnair@icloud.com>
2019-07-12 01:14:18 +0530

    fixing dist file location based on angular json

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname+ '/dist'))
-const port = process.env.PORT || 8080;
+app.use(express.static(__dirname+ '/dist/flopkart'))
+let port = process.env.PORT || 8080;
 app.listen(port)
 
 app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/flopkart/index.html'));
 })
 
 console.log('Express listening to :'+port);
